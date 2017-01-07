b4b392934deb380147bc1e1f69ed4a3b5cfd4ae0
Denis Trofimov <silaradost@yandex.ru>
2017-01-07 20:54:14 +0400

    exercises 6 & 7

---
+++ bjarnestroustrup4_11_7.cpp
@@ -0,0 +1,34 @@
+/*
+ * bjarnestroustrup4_11_7.cpp
+ *
+ * Copyright 2017 Denis Trofimov <silaradost@yandex.ru>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *  python wsgi unittest html css debian linux postgresql sqllite mvc extjs4 simpletal bpmn idef0 git vim planner redmine uml otrs
+ * c++ cmake stl html css fedora linux oracle dba mysql ui xml xslt bpmn idef0 git sql emacs vim kdevelop ms-visio ms-word cvs
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "bool->char->short->int->float->double\n";
+    cout << "bool->char->int->double\n";
+    return 0;
+}
+
